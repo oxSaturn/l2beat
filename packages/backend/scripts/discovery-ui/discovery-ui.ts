d18c1ec66cb51846b2c93d134ca3b505e8c5f4b5
Adrian Adamiak <adrian@adamiak.net>
2024-09-15 12:44:14 +0000

    Show projects list

--- packages/backend/scripts/discovery-ui/discovery-ui.ts
+++ packages/backend/scripts/discovery-ui/discovery-ui.ts
@@ -1,10 +1,13 @@
 import path from 'path'
+import { ConfigReader } from '@l2beat/discovery'
 import express from 'express'
 import { engine } from 'express-handlebars'
 import { getSSEDiv, streamCLI } from './streamCLI'
 
 export const app = express()
 const port = 3000
+
+const configReader = new ConfigReader()
 
 // Set up Handlebars
 app.engine('handlebars', engine())
@@ -17,6 +20,12 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (_, res) => {
   res.render('home')
+})
+
+// get url /project/:chain/ and render home.handlebars, passing chain
+app.get('/projects/:chain/:project', (req, res) => {
+  const { chain, project } = req.params
+  res.render('project', { chain, project })
 })
 
 app.get('/greet', (req, res) => {
@@ -39,6 +48,22 @@ app.get('/start-command', (req, res) => {
 app.get('/stream-command', (req, res) => {
   // streamCLI(req, res, 'ls', ['-la'])
   streamCLI(req, res, 'yarn', ['discover', 'ethereum', 'zora', '--dev'])
+})
+
+app.get('/partials/projects-list', async (_, res) => {
+  const chainConfigs = await Promise.all(
+    configReader
+      .readAllChains()
+      .flatMap((chain) => configReader.readAllConfigsForChain(chain)),
+  )
+  const projects = chainConfigs.map((config) => ({
+    name: config.name,
+    chain: config.chain,
+  }))
+
+  res.render('partials/projectsList', { projects }, (_err, html) => {
+    res.send(html)
+  })
 })
 
 app.listen(port, () => {
